685c82b30817ed35fdee6e84961df739b97838ad
Michalis Christou <73609415+MichailChristou@users.noreply.github.com>
2021-01-19 19:02:30 +0200

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,19 @@
+import json
+import urllib.request
+
+data=json.load(open('dictionary.json'))
+
+# loop for every crypto coin in dictionary
+for keys in data:
+    #construction url to dict
+    url="https://min-api.cryptocompare.com/data/pricemulti?fsyms="+keys+"&tsyms=EUR&e=CCCAGG"
+    r=urllib.request.urlopen(url)
+    html=r.read()
+    html=html.decode()
+    prices=json.loads(html)
+    #isolation of prices and values
+    moneydict=(prices[keys])
+    eur=(moneydict["EUR"])
+    val=(data[keys])
+    print("Η αξία του",keys,"είναι: ")
+    print(int(val)*int(eur))
